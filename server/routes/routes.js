81588be86894b67912532ae0b1abbc9ff0f27b2d
danielufeli <danielufeli@yahoo.com>
2019-05-11 13:19:17 +0100

    edited travis yml file, added two modules to transpile mocha test in babel

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,12 +4,10 @@ import users from './users';
 import adminusers from './adminusers';
 import loans from './loans';
 import swaggerSpec from '../../config/swagger';
-import homepage from './homepage';
 
 export default (app) => {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
-  app.use('/', homepage);
   app.use('/api/v1/auth', users);
   app.use('/api/v1/users', adminusers);
   app.use('/api/v1/loans', loans);
