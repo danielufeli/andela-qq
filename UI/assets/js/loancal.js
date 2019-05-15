7e0011c2800186cd5db9a620ebdf9b55bfeabd30
danielufeli <danielufeli@yahoo.com>
2019-05-15 05:29:09 +0100

    Update UI on ghpages

--- UI/assets/js/loancal.js
+++ UI/assets/js/loancal.js
@@ -3,21 +3,20 @@
  *
  */
 const calculate = () => {
-  const amount = Number(document.getElementById("amount").value);
-  const months = Number(document.getElementById("months").value);
+  const amount = Number(document.getElementById('amount').value);
+  const months = Number(document.getElementById('months').value);
   const interest = parseFloat(5 / 100) * amount;
   const payments = (amount + interest) / months;
   const totalpayable = payments * months;
   const totalinterest = totalpayable - amount;
 
   if (isFinite(payments)) {
-    console.log(payments);
-    document.getElementById("payment").innerHTML = payments;
-    document.getElementById("total").innerHTML = totalpayable;
-    document.getElementById("totalinterest").innerHTML = totalinterest;
+    document.getElementById('payment').innerHTML = payments;
+    document.getElementById('total').innerHTML = totalpayable;
+    document.getElementById('totalinterest').innerHTML = totalinterest;
   } else {
-    document.getElementById("payment").innerHTML = 0.0;
-    document.getElementById("total").innerHTML = "0.00";
-    document.getElementById("totalinterest").innerHTML = "0.00";
+    document.getElementById('payment').innerHTML = 0.0;
+    document.getElementById('total').innerHTML = '0.00';
+    document.getElementById('totalinterest').innerHTML = '0.00';
   }
 };
